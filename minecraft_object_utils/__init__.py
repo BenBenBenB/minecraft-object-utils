06e910b14594470921167023aa6a224a047d6360
Ben Burton <benjaminburton@gmail.com>
2023-08-23 23:00:21 -0400

    appease isort

--- minecraft_object_utils/__init__.py
+++ minecraft_object_utils/__init__.py
@@ -1,5 +1,5 @@
 from .block_factory import Block, BlockFactory, BlockProperty, BlockTraits  # noqa: F401
 from .entity_factory import Entity, EntityFactory, EntityTraits  # noqa: F401
-from .item_factory import ItemStack, ItemFactory, ItemTraits  # noqa: F401
+from .item_factory import ItemFactory, ItemStack, ItemTraits  # noqa: F401
 from .minecraft_object_factory import MinecraftObjectFactory  # noqa: F401
 from .mod_info import VANILLA_JAVA_LATEST, ModInfo  # noqa: F401
